be3e2eee00b3e67dd289b19d8f033b076625528c
Ahmad <ahmadalteneh1@gmail.com>
2024-12-22 21:40:46 -0600

    Added the cartSlice Reducers in the cartslice.jsx file

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,25 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const existingIem = state.items.find(item => item.id === name);
+      if (existingIem) {
+        existingIem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 })
+      }
+
     },
     removeItem: (state, action) => {
+      state.item = state.item.filer(item => item.name !== action.payload)
     },
     updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
 
-    
     },
   },
 });
